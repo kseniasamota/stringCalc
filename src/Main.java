72db3f2fd2a8fdc91bc1d4df20b6653dbdf7495d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-02-13 20:25:08 +0300

    added method calc, get sign

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input){
+        String answer = new String();
+        char[] charInput = input.toCharArray();
+        int firstNumber;
+        int secondNumber;
+        Character currentSign = ' ';
+
+        for(int i = 0; i < charInput.length; i++){
+            if(charInput[i] == '+' || charInput[i] == '-' || charInput[i] == '*' || charInput[i] == '/'){
+                currentSign = charInput[i];
+            }
+        }
+
+        String[] component = input.split(" - ");
+
+        firstNumber = Integer.parseInt(component[0]);
+        secondNumber= Integer.parseInt(component[1]);
+
+        answer = Integer.toString(firstNumber - secondNumber);
+
+
+
+        return answer;
     }
 }
